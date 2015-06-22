86d4c38351ed95482fa33ea1b4aab837e8f65452
Kristina Brown <kristina.frey.frey@gmail.com>
2015-06-21 21:28:55 -0600

    logo stuff, and favicon added

--- app/assets/javascripts/video.js
+++ app/assets/javascripts/video.js
@@ -1,22 +1,28 @@
+
 var tag = document.createElement('script');
- tag.src = "http://www.youtube.com/player_api";
- var firstScriptTag = document.getElementsByTagName('script')[0];
- firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+tag.src = "http://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
  // 3. This function creates an <iframe> (and YouTube player)
  //    after the API code downloads.
- var player;
- function onYouTubePlayerAPIReady() {
-   player = new YT.Player('player', {
-     playerVars: { 'autoplay': 1, 'controls': 1,'autohide':1,'wmode':'opaque' },
-     videoId: '2Tx3wm8mN6g',
-     events: {
-       'onReady': onPlayerReady}
-   });
- }
- 
+var player;
+function onYouTubePlayerAPIReady() {
+ player = new YT.Player('player', {
+   playerVars: { 'autoplay': 1, 'controls': 1,'autohide':1,'wmode':'opaque' },
+   videoId: '2Tx3wm8mN6g',
+   events: {
+     'onReady': onPlayerReady}
+ });
+}
 
- // 4. The API will call this function when the video player is ready.
- function onPlayerReady(event) {
-   event.target.mute();
- }
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+ event.target.mute();
+}
+
+ //tWxgxIRCCqQ walking park
+ 
+ //2Tx3wm8mN6g subaru commercial 
+ 
